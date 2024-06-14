739709b0bbc9e8d7ef2a82ca6d9a1487756fec99
SETEC <setec@Mac-mini-de-SETEC.local>
2024-06-14 17:15:49 -0300

    Login Social - Google

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -5,8 +6,6 @@ import theme from './src/theme';
 
 import { SignIn } from "./src/screens/SignIn";
 import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
-
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,54 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import { Container, Title, Slogan } from './styles';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 import backgroundImg from '../../assets/background.png';
 
 import { Button } from '../../components/Button';
 
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID
+})
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true)
+
+      const { idToken } = await GoogleSignin.signIn()
+
+      if (idToken) {
+
+      } else {
+        Alert.alert("Entrar", "Não foi possível conectar-se a sua conta Google.")
+        setIsAuthenticating(false)
+      }
+      
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Entrar", "Não foi possível conectar-se a sua conta Google.")
+      setIsAuthenticating(false)
+    }
+  }
+
   return (
     <Container source={backgroundImg} >
       <Title>Ignite Fleet</Title>
+
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button title="Entrar com Google" />
+
+      <Button 
+        title="Entrar com Google" 
+        isLoading={isAuthenticating} 
+        onPress={handleGoogleSignIn} 
+      />
     </Container>
   );
 }
